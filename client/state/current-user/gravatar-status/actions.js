57f73ef2db8d3b8e2e3b5f6b22afa0bda90e1e27
Payton Swick <payton@foolord.com>
2017-07-01 14:35:48 -0400

    Remove thunk from uploadGravatar action

--- client/state/current-user/gravatar-status/actions.js
+++ client/state/current-user/gravatar-status/actions.js
@@ -1,17 +1,9 @@
 /**
- * External dependencies
- */
-import wpcom from 'lib/wp';
-
-/**
  * Internal dependencies
  */
 import {
 	GRAVATAR_RECEIVE_IMAGE_FAILURE,
-	GRAVATAR_UPLOAD_RECEIVE,
 	GRAVATAR_UPLOAD_REQUEST,
-	GRAVATAR_UPLOAD_REQUEST_SUCCESS,
-	GRAVATAR_UPLOAD_REQUEST_FAILURE
 } from 'state/action-types';
 import {
 	bumpStat,
@@ -21,36 +13,10 @@ import {
 } from 'state/analytics/actions';
 
 export function uploadGravatar( file, email ) {
-	return dispatch => {
-		dispatch( withAnalytics(
-			recordTracksEvent( 'calypso_edit_gravatar_upload_start' ),
-			{ type: GRAVATAR_UPLOAD_REQUEST }
-		) );
-		return wpcom.undocumented().uploadGravatar( email, file )
-			.then( () => {
-				const fileReader = new FileReader( file );
-				fileReader.addEventListener( 'load', function() {
-					dispatch( {
-						type: GRAVATAR_UPLOAD_RECEIVE,
-						src: fileReader.result,
-					} );
-					dispatch( withAnalytics(
-						recordTracksEvent( 'calypso_edit_gravatar_upload_success' ),
-						{ type: GRAVATAR_UPLOAD_REQUEST_SUCCESS }
-					) );
-				} );
-				fileReader.readAsDataURL( file );
-			} )
-			.catch( () => {
-				dispatch( withAnalytics(
-					composeAnalytics(
-						recordTracksEvent( 'calypso_edit_gravatar_upload_failure' ),
-						bumpStat( 'calypso_gravatar_update_error', 'unsuccessful_http_response' )
-					),
-					{ type: GRAVATAR_UPLOAD_REQUEST_FAILURE }
-				) );
-			} );
-	};
+	return withAnalytics(
+		recordTracksEvent( 'calypso_edit_gravatar_upload_start' ),
+		{ type: GRAVATAR_UPLOAD_REQUEST, file, email }
+	);
 }
 
 export const receiveGravatarImageFailed = ( { errorMessage, statName } ) =>
